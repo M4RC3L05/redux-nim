750baec7886da9450073b26b4882fabdde5afb3b
M4RC3L0 <joaobat@live.com.pt>
2018-10-20 13:05:01 +0100

    update docs

--- genDocs.sh
+++ genDocs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-nim doc2 --docSeeSrcUrl:txt ./src/redux_nim.nim
-# rm -rf docs
-# mkdir docs
-# mv src/htmldocs/* docs
-# mv docs/redux_nim.html docs/index.html
-# rm -rf src/htmldocs
+nim doc --project src/redux_nim.nim
+rm -rf docs
+mkdir docs
+mv src/htmldocs/* docs
+mv docs/redux_nim.html docs/index.html
+rm -rf src/htmldocs
